df15463759ed026f1dd7344e3b2ad45dbb0dac1f
HackeruApps <appdevhack@gmail.com>
2018-11-11 14:52:43 +0200

    update

--- Lec8/Lec8/ViewControllers/MoviesTableViewController.swift
+++ Lec8/Lec8/ViewControllers/MoviesTableViewController.swift
@@ -8,11 +8,21 @@
 
 import UIKit
 
-class MoviesTableViewController: UITableViewController {
-
+class MoviesTableViewController: UITableViewController, MovieDataSourceDelegate {
+    
+    var movies: [Movie]?
+    //delegate
+    func moviesArrived(rss: RssResult) {
+        self.movies = rss.feed.results
+        self.tableView.reloadData()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        let ds = MovieDataSource()
+        ds.delegate = self
+        ds.getMovies()
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
@@ -29,23 +39,23 @@ class MoviesTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return movies?.count ?? 0
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "movieCell", for: indexPath)
 
         // Configure the cell...
 
         return cell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
