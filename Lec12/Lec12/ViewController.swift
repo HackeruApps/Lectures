cbc43e2eba0e307385ee32e55e1729d3a14feaac
HackeruApps <appdevhack@gmail.com>
2018-11-25 13:21:13 +0200

    keychainDemoLogin

--- Lec12/Lec12/ViewController.swift
+++ Lec12/Lec12/ViewController.swift
@@ -1,7 +1,74 @@
 import UIKit
 
 class ViewController: UIViewController {
- 
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginOrRegisterSwitch: UISwitch!
+    @IBOutlet weak var loginRegButton: UIButton!
+    
+    
+    var isLogin:Bool{
+        return loginOrRegisterSwitch.isOn
+    }
+    
+    @IBAction func loginORRegister(_ sender: UISwitch) {
+        loginRegButton.setTitle(sender.isOn ? "Login" : "Reigster", for: .normal)
+    }
+    
+    @IBAction func buttontapped(_ sender: UIButton) {
+        if !nameTextField.isValid{
+            nameTextField.layer.borderColor = UIColor.red.cgColor
+            nameTextField.layer.borderWidth = 3
+            nameTextField.layer.masksToBounds = true
+            nameTextField.layer.cornerRadius = 14
+        }
+        
+        if !nameTextField.isValid || !passwordTextField.isValid{
+            return
+        }
+        
+        
+        if isLogin{
+            login()
+        }else{
+            register()
+        }
+    }
+    
+    func login(){
+        let name = nameTextField.text!
+        let key = KeychainPasswordItem(service: "Lec12", account: name)
+        
+        do{
+            let keyChainPass = try key.readPassword()
+            
+            if keyChainPass == passwordTextField.text!{
+                performSegue(withIdentifier: "login", sender: nil)
+            }
+        }catch{
+            fatalError("Error: \(error)")
+        }
+        
+        
+    }
+    
+    func register(){
+        //Service is the app name
+        
+        let name = nameTextField.text!
+        
+        let key = KeychainPasswordItem(service: "Lec12", account: name)
+        
+        do{
+            try key.savePassword(passwordTextField.text!)
+        }catch {
+            fatalError("Error Saving to Keychain \(error)" )
+        }
+        
+        performSegue(withIdentifier: "login", sender: nil)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -48,6 +115,29 @@ class ViewController: UIViewController {
 
     
 }
+
+extension UITextField{
+    var isValid:Bool{
+        return hasText && self.text?.count ?? 0 > 3
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
